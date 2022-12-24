6f6cd5729cd7f083b80062de52fa46f5e7e8d7af
[Ali Karimzade] <[alikarimzade999@gmail.com]>
2022-12-24 08:24:27 -0500

    [adding git to the project]

---
+++ cnn_AlexNet_cifar10.py
@@ -0,0 +1,180 @@
+import time
+import os
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import numpy as np
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# these 5 line of code added to resolve an error
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+###############################################
+#loading the dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+assert x_train.shape == (50000, 32, 32, 3)
+assert x_test.shape == (10000, 32, 32, 3)
+assert y_train.shape == (50000, 1)
+assert y_test.shape == (10000, 1)
+# name of dataset classes
+CLASS_NAMES = ['airplane', 'automobile', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+# splite 5k of samply for validation during the training
+x_val, y_val = x_train[:5000], y_train[:5000]
+x_train, y_train = x_train[5000:], y_train[5000:]
+
+# creating tensorflow Dataset
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+validation_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+
+#plotting some samples of the data
+plt.figure(figsize=(20, 20))
+print(train_ds)
+for i, (image, label) in enumerate(train_ds.take(25)):
+    ax = plt.subplot(5, 5, i+1)
+    plt.imshow(image)
+    plt.title(CLASS_NAMES[label.numpy()[0]])
+    plt.axis('off')
+
+plt.show()
+
+##preproccessing the data
+#Normalizing and standardizing the images
+def process_images(image, label):
+    # Normalize images to have a mean of 0 and standard deviation of 1
+    image = tf.image.per_image_standardization(image)
+    # Resize images from 32x32 to 64x64
+    image = tf.image.resize(image, (64, 64))
+    return image, label
+
+#getting the size of the data to ensure about it before passing through the network.
+train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
+test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
+validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
+print("Training data size:", train_ds_size)
+print("Test data size:", test_ds_size)
+print("Validation data size:", validation_ds_size)
+
+#Preprocessing the data within the dataset
+#Shuffling the dataset
+#Batch data within the dataset with size 32
+
+train_ds = (train_ds
+            .map(process_images)
+            #.map(lambda x,y:(tf.cast(x, tf.float16),y))
+            .shuffle(buffer_size=train_ds_size)
+            .batch(batch_size=32, drop_remainder=True))
+test_ds = (test_ds
+           .map(process_images)
+           #.map(lambda x,y:(tf.cast(x, tf.float16),y))
+           .shuffle(buffer_size=train_ds_size)
+           .batch(batch_size=32, drop_remainder=True))
+validation_ds = (validation_ds
+                 .map(process_images)
+                 #.map(lambda x,y:(tf.cast(x, tf.float16),y))
+                 .shuffle(buffer_size=train_ds_size)
+                 .batch(batch_size=32, drop_remainder=True))
+
+# creating the model to be look like Alexnet CNN architecture
+
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=96, kernel_size=(11, 11), strides=(
+        1, 1), activation='relu', input_shape=(64, 64, 3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Conv2D(filters=256, kernel_size=(
+        5, 5), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Conv2D(filters=384, kernel_size=(
+        3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=384, kernel_size=(
+        3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=256, kernel_size=(
+        3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation='softmax')
+])
+# I tried two different optimizer but this one(SGD) worked better with hier speed and I add accurecy to the metrics
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=tf.optimizers.SGD(lr=0.001), metrics=['accuracy'])
+#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+# implementing a EarlyStopping and saving model in lowest val_loss
+earlyStopping = EarlyStopping(monitor='val_loss', patience=6, verbose=0, mode='min')
+mcp_save = ModelCheckpoint('./model.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+
+#training the model
+history =model.fit(train_ds,
+                   epochs=50,
+                   validation_data=validation_ds,
+                   validation_freq=1,
+                   callbacks=[earlyStopping, mcp_save])
+
+# evaluating the model base on test dataset
+model.evaluate(test_ds)
+
+# plotting the accuracy and loss for validation and training datasets
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('model accuracy & loss')
+ 
+ax1.plot(history.history['accuracy'])
+ax1.plot(history.history['val_accuracy'])
+ax1.set_title('model accuracy')
+ax1.set_ylabel('accuracy')
+ax1.set_xlabel('epoch')
+ax1.legend(['train', 'val'], loc='upper left')
+
+ax2.plot(history.history['loss'])
+ax2.plot(history.history['val_loss'])
+ax2.set_title('model loss')
+ax2.set_ylabel('loss')
+ax2.set_xlabel('epoch')
+ax2.legend(['train', 'val'], loc='upper left')
+
+plt.show()
+
+#loading the model by weights
+
+model2 = keras.models.Sequential([
+    keras.layers.Conv2D(filters=96, kernel_size=(11, 11), strides=(
+        1, 1), activation='relu', input_shape=(64, 64, 3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Conv2D(filters=256, kernel_size=(
+        5, 5), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Conv2D(filters=384, kernel_size=(
+        3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=384, kernel_size=(
+        3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=256, kernel_size=(
+        3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation='softmax')
+])
+model2.load_weights("./model.hdf5")
